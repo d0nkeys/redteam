12302c1a7504b3afdfb19bfc483d448dc490d57f
Francesco Soncina <phra@users.noreply.github.com>
2019-03-05 00:04:09 +0100

    Update winrm-fs.rb

--- lateral-movement/winrm-fs.rb
+++ lateral-movement/winrm-fs.rb
@@ -27,7 +27,7 @@ class String
     end
 end
 
-reader = TTY::Reader.new
+reader = TTY::Reader.new(history_duplicates: true)
 
 reader.on(:keyctrl_x) { puts ""; puts "Exiting..."; exit }
 
@@ -36,11 +36,7 @@ command=""
 conn.shell(:powershell) do | shell |
     until command == "exit\n" do
         begin
-            #output = shell.run("-join($id,'PS ',$(whoami),'@',$env:computername,' ',$((gi $pwd).Name),'> ')")
-            #output = shell.run("-join($id,'PS ',$((gi $pwd)),'> ')")
-            output = shell.run("-join($id,'PS ',$pwd,'> ')")
-            #print(output.output.chomp)
-            #command = gets
+            output = shell.run("-join($id,'PS ',$(whoami),'@',$env:computername,' ',$pwd,'> ')")
             command = reader.read_line(output.output.chomp)
 
             if command.start_with?('UPLOAD') then
