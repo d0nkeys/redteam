2f347c586ba5b821ec1eb0b94f60376b1ebd569b
Francesco Soncina <phra@users.noreply.github.com>
2019-03-04 23:08:16 +0100

    Update winrm-fs.rb

--- lateral-movement/winrm-fs.rb
+++ lateral-movement/winrm-fs.rb
@@ -2,15 +2,16 @@
 
 require 'winrm-fs'
 
-# Author: Alamot, Lupman
+# Author: Alamot
 # To upload a file type: UPLOAD local_path remote_path
 # e.g.: PS> UPLOAD myfile.txt C:\temp\myfile.txt
-# e.g.: PS> DOWNLOAD C:\temp\myfile.txt myfile.txt
 
-conn = WinRM::Connection.new(
-  endpoint: 'http://localhost:5985/wsman',
-  user: 'user',
-  password: 'password'
+conn = WinRM::Connection.new( 
+    endpoint: 'http://localhost:5985/wsman',
+    user: 'user',
+    password: 'password',
+    #transport: :ssl,
+    :no_ssl_peer_verification => true
 )
 
 file_manager = WinRM::FS::FileManager.new(conn)
@@ -35,8 +36,14 @@ conn.shell(:powershell) do | shell |
 
             if command.start_with?('UPLOAD') then
                 upload_command = command.tokenize
-                puts("Uploading " + upload_command[1] + " to " + (upload_command[2] || upload_command[1]))
-                file_manager.upload(upload_command[1], upload_command[2] || upload_command[1]) do | bytes_copied, total_bytes, local_path, remote_path |
+                dest = upload_command[2]
+                if not dest then
+                    dest = upload_command[1].split('/')[-1]
+                    output = shell.run("-join($pwd)")
+                    dest = output.output.chomp.strip() + "\\" + dest
+                end
+                puts("Uploading " + upload_command[1] + " to " + dest)
+                file_manager.upload(upload_command[1], dest) do | bytes_copied, total_bytes, local_path, remote_path |
                     puts("#{bytes_copied} bytes of #{total_bytes} bytes copied")
                 end
                 command = "echo OK`n"
@@ -44,8 +51,12 @@ conn.shell(:powershell) do | shell |
 
             if command.start_with?('DOWNLOAD') then
                 download_command = command.tokenize
-                puts("Downloading " + download_command[1] + " to " + (download_command[2] || download_command[1]))
-                file_manager.download(download_command[1], download_command[2] || download_command[1]) do | bytes_copied, total_bytes, remote_path, local_path |
+                dest = upload_command[2]
+                if not dest then
+                    dest = upload_command[1].split('\\')[-1]
+                end
+                puts("Downloading " + download_command[1] + " to " + dest)
+                file_manager.download(download_command[1], dest) do | bytes_copied, total_bytes, remote_path, local_path |
                     puts("#{bytes_copied} bytes of #{total_bytes} bytes copied")
                 end
                 command = "echo OK`n"
